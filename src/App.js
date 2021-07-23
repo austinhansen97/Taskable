feda2bbb1e83b9c8a1106d35c585700faca7db04
unknown <austinhansen97@gmail.com>
2021-07-23 11:46:01 -0600

    Just making sure Taskable is pushing separately from EstatePal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import TileArea from "./components/TileArea";
 import "./components/TileArea.css";
 
 const dataObject = [
-  { task: "Title", description: "Description of task"},
+  { task: "Title", description: "Description of task" },
   // { name: "Tom", job: "Smith's", hobby: "Swimming" },
   // { name: "Kyle", job: "Chevron", hobby: "Hanging with friends" },
-  // { name: "Ellie", job: "Joe's Crabshack", hobby: "Running" },
+  // { name: "Cindy", job: "Joe's Crabshack", hobby: "Running" },
   // { name: "Kelly", job: "Winco", hobby: "Gaming" },
 ];
 
@@ -14,7 +14,6 @@ function App() {
   const [data, newData] = useState(dataObject);
 
   function addCardHandler(newCardArray) {
-    
     newData((previousData) => {
       return [newCardArray, ...previousData];
     });
